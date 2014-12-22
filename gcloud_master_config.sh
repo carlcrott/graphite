e67f6299e7c41930508d5a97f9c4b118cfa51174
Carl Crott <carlcrott@gmail.com>
2014-12-22 12:15:29 -0800

    troubleshooting

--- gcloud_master_config.sh
+++ gcloud_master_config.sh
@@ -91,6 +91,49 @@ cp ~/graphite/salt/minion /etc/salt/
 mkdir -p /srv/salt/
 cp ~/graphite/salt/*.sls /srv/salt/
 
+# default location for salt state files
+mkdir -p /srv/pillar/
+
+envsubst <<EOF > /srv/pillar/top.sls
+base:
+  '*-monitor':
+    - graphite
+EOF
+
+envsubst <<EOF > /srv/pillar/graphite.sls
+# this example illustrates key you can overwrite in pillar (or grains)
+# the values given here are the default ones already included
+
+graphite:
+  config:
+    port: 2003
+    pickle_port: 2004
+    dbtype: sqlite3
+    dbname: '/opt/graphite/storage/graphite.db'
+    dbuser: 'test'
+    dbpassword: 'test'
+    dbhost: '127.0.0.1'
+    dbport: '8080'
+    admin_user: 'admin'
+    admin_password: 'pbkdf2_sha256$10000$wZuRMciV2VKr$OAtsP+BksbR2DPQUEsY728cbIJmuYf4uXg4tLLGsvi4='
+    admin_email: 'admin@example.com'
+    max_creates_per_minute: 50
+    max_updates_per_second: 500
+
+# a mysql example
+    #dbtype: mysql
+    #dbname: 'graphite'
+    #dbuser: 'graphite'
+    #dbpassword: 'graphite'
+    #dbhost: 'localhost'
+    #dbport: '3306'
+    #admin_user: 'admin'
+    #admin_password: 'pbkdf2_sha256$10000$wZuRMciV2VKr$OAtsP+BksbR2DPQUEsY728cbIJmuYf4uXg4tLLGsvi4='
+    #admin_email: 'admin@example.com'
+EOF
+
+
+
 #mkdir -p /srv/
 
 #mkdir /etc/salt/
