c6400eb936bfa8bb4204a7912e594bd5de25d603
Carl Crott <carlcrott@gmail.com>
2015-01-02 10:44:56 -0800

    working towards mysql default monitoring

--- gcloud_master_config.sh
+++ gcloud_master_config.sh
@@ -113,16 +113,14 @@ cp ~/graphite/salt/master /etc/salt/master
 # Place minion config
 cp ~/graphite/salt/minion /etc/salt/minion
 
+# Default location for salt state files
+mkdir -p /srv/salt/
+cp ~/graphite/salt/*.sls /srv/salt/
 
 # Default formula dir structure and parents
 mkdir -p /srv/formulas/ && cd /srv/formulas/
 # Clone the graphite formula
 git clone git@github.com:carlcrott/graphite-formula.git
-
-
-# Default location for salt state files
-mkdir -p /srv/salt/
-cp ~/graphite/salt/*.sls /srv/salt/
 
 # Default location for salt state files
 mkdir -p /srv/pillar/
